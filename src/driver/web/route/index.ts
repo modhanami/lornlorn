219a059ebbfeec9b5c970a29c97daeab19b155a6
Rooted <modhanami@gmail.com>
2022-01-05 13:41:27 +0700

    Add routes + composition root

---
+++ src/driver/web/route/index.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from "@prisma/client";
+import { Router } from "express";
+import { createAuthenticationService } from "../../../application/service/authentication";
+import { createCartService } from "../../../application/service/cart";
+import { createPasswordService } from "../../../application/service/password";
+import { createProductService } from "../../../application/service/product";
+import { createTokenService } from "../../../application/service/token";
+import { createUserService } from "../../../application/service/user";
+import { createCartAdapter } from "../../../infrastructure/prisma/cart";
+import { createProductAdapter } from "../../../infrastructure/prisma/product";
+import { createUserAdapter } from "../../../infrastructure/prisma/user";
+import { createCartController } from "../controller/cart";
+import { createProductController } from "../controller/product";
+import { createUserController } from "../controller/user";
+import { createAuthMiddlewares } from "../middleware";
+import errorHandler from "../middleware/defaultErrorHandler";
+
+const prisma = new PrismaClient();
+const userGateway = createUserAdapter(prisma);
+const productGateway = createProductAdapter(prisma);
+const cartGateway = createCartAdapter(prisma);
+
+const passwordService = createPasswordService();
+const tokenService = createTokenService();
+const userService = createUserService(userGateway);
+const productService = createProductService(productGateway);
+const cartService = createCartService(cartGateway, userGateway, productGateway);
+const authenticationService = createAuthenticationService(userGateway, passwordService, tokenService);
+
+const userControler = createUserController(userService, authenticationService, passwordService);
+const productController = createProductController(productService);
+const cartController = createCartController(cartService);
+
+const router = Router();
+const userRouter = Router();
+const productRouter = Router();
+const cartRouter = Router();
+
+const authMiddlewares = createAuthMiddlewares(tokenService);
+
+router.use("/users", userRouter);
+userRouter.post("/", userControler.signup);
+userRouter.post("/login", userControler.login);
+
+router.use("/products", productRouter);
+productRouter.get("/", productController.findAll);
+productRouter.get("/:id", productController.findById);
+
+router.use("/carts", cartRouter);
+cartRouter.post("/me/add-item", authMiddlewares.verifyToken, cartController.addOwnCartItem);
+
+router.use(errorHandler);
+
+export default router;
