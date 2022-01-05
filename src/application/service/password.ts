9d87f8eab8b8a1373769b8e60551a5307c932eec
Rooted <modhanami@gmail.com>
2022-01-05 12:59:10 +0700

    Export createPasswordService()

--- src/application/service/password.ts
+++ src/application/service/password.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import { PasswordUseCase } from '../../application/ports';
 
-function createPasswordService(): PasswordUseCase {
+export function createPasswordService(): PasswordUseCase {
   return {
     async compare(candidate, hashedPassword) {
       return bcrypt.compare(candidate, hashedPassword);
