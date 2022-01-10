f5caf15e58ca67601b105439b625912eca3dfcca
Rooted <modhanami@gmail.com>
2022-01-10 22:10:48 +0700

    Add refresh token to domain

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -1,10 +1,11 @@
-import { UserEmail, UniqueId, UserUsername } from "./sharedKernel";
+import { UniqueId, UserEmail, UserToken, UserUsername } from "./sharedKernel";
 
 export type User = {
   id?: UniqueId;
   username: UserUsername;
   password: UserPassword;
   email: UserEmail;
+  refreshToken?: UserToken;
 };
 
 export type UserPassword = string;
