2a5b2c0900577790a71363d2300ac2d8b93eeff9
Rooted <modhanami@gmail.com>
2022-01-10 22:40:58 +0700

    Add CORS for frontend (support XD cookies)

--- src/driver/web/app.ts
+++ src/driver/web/app.ts
@@ -1,12 +1,17 @@
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 import errorHandler from './middleware/defaultErrorHandler';
 import router from './route';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL || undefined,
+  credentials: true,
+}));
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 
