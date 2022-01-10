fc64995734c95462e10a85b5884401f16f77cfc1
Rooted <modhanami@gmail.com>
2022-01-10 22:52:53 +0700

    Update default error handler

--- src/driver/web/middleware/defaultErrorHandler.ts
+++ src/driver/web/middleware/defaultErrorHandler.ts
@@ -1,18 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 
 function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  console.log(`${new Date()} - ${req.originalUrl}`);
   if (res.headersSent) {
     return next(err);
   }
 
   if (err instanceof Error) {
+    console.log(err.stack);
     return res.status(500).send({
-      message: err.message,
+      error: {
+        name: err.name,
+        message: err.message,
+      }
     });
   }
 
   return res.status(500).send({
-    message: 'Internal server error',
+    error: {
+      message: 'Internal server error',
+    }
   });
 }
 
