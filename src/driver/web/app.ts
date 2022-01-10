b910c995a37927358e40e451767284bea315540f
Rooted <modhanami@gmail.com>
2022-01-10 22:37:06 +0700

    Update app's middlewares

--- src/driver/web/app.ts
+++ src/driver/web/app.ts
@@ -1,11 +1,14 @@
-import express from 'express';
-import router from './route';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import express from 'express';
 import errorHandler from './middleware/defaultErrorHandler';
+import router from './route';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use('/api', router);
 app.use(errorHandler);
